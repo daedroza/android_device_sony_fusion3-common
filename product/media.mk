7331f8ce0430e0c3f3f33da5ac855ef417443e68
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    media: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    Includes commit from marlin:
    'Remove the property media.settings.xml.' (4c9b629e)
    
    Merged-In: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

--- product/media.mk
+++ product/media.mk
@@ -2,4 +2,4 @@
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(COMMON_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+    $(COMMON_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
