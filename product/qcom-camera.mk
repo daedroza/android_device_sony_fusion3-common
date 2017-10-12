012f0c0a25db2b0afcd7e53dbbf4c7d534884951
Ethan Chen <intervigil@gmail.com>
2017-10-11 20:53:37 -0700

    hidl: Use Lineage legacy camera provider
    Change-Id: I73e072e70d89c43da0e4eb7bf8b53757e0c15a85

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -4,5 +4,5 @@ PRODUCT_COPY_FILES += \
 
 # Camera HIDL interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy
