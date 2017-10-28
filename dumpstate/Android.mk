LineageOS/android_device_sony_huashan

dumpstate: Convert to hidl implementation
Based on implementation for angler (7fab099).

Includes:
"Uses libdumpstateheaders instead of LOCAL_C_INCLUDES" (60e7a0f)
"Uses new Dumpstate class functions on dumpstate_board()" (d75e6a6)
"Mark hal_dumpstate_impl as HAL impl domain" (66957ed)
"Switch Dumpstate HAL to hal_server_domain " (c3b9841)
"dumpstate HAL uses "default" service name()" (5b8189a)
"Check result of registerAsService in dumpstate." (cf2996a)

Change-Id: I91e0caeb4964a85e8d43d2aad86acd65b1cc2488

dumpstate/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,15 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.dumpstate@1.0-service.huashan
+LOCAL_INIT_RC := android.hardware.dumpstate@1.0-service.huashan.rc
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := \
+    DumpstateDevice.cpp \
+    service.cpp
 
-LOCAL_C_INCLUDES := frameworks/native/cmds/dumpstate
-
-LOCAL_SRC_FILES := dumpstate.c
-
-LOCAL_MODULE := libdumpstate.sony
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.dumpstate@1.0 \
+    libbase \
+    libcutils \
+    libdumpstateutil \
+    libhidlbase \
+    libhidltransport \
+    libhwbinder \
+    liblog \
+    libutils
 
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_EXECUTABLE)

product/platform.mk
@@ -14,3 +14,7 @@ PRODUCT_PACKAGES += \
 # Sony MACAddrSetup package
 PRODUCT_PACKAGES += \
     macaddrsetup
+
+# Dumpstate HAL
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-service.huashan

daedroza/android_device_sony_fusion3-common

dumpstate/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,15 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.dumpstate@1.0-service.fusion3
+LOCAL_INIT_RC := android.hardware.dumpstate@1.0-service.fusion3.rc
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := \
+    DumpstateDevice.cpp \
+    service.cpp
 
-LOCAL_C_INCLUDES := frameworks/native/cmds/dumpstate
-
-LOCAL_SRC_FILES := dumpstate.cpp
-
-LOCAL_MODULE := libdumpstate.sony
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.dumpstate@1.0 \
+    libbase \
+    libcutils \
+    libdumpstateutil \
+    libhidlbase \
+    libhidltransport \
+    libhwbinder \
+    liblog \
+    libutils
 
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_EXECUTABLE)

product/platform.mk
@@ -10,3 +10,7 @@ PRODUCT_COPY_FILES += \
 # Sony MACAddrSetup package
 PRODUCT_PACKAGES += \
     macaddrsetup
+
+# Dumpstate HAL
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-service.fusion3